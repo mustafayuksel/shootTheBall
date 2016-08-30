cd887117031105878e68b69f6b4cbf2f20294895
Mustafa <mustafa@mustafas-mbp.mynet>
2016-08-30 21:13:16 +0300

    level added v2

--- Assets/ShootTheBall/Scripts/LevelManager.cs
+++ Assets/ShootTheBall/Scripts/LevelManager.cs
@@ -49,7 +49,6 @@ public class LevelManager : MonoBehaviour {
 			.setRing1AnimType(EGTween.EaseType.linear).setRing1MinSpeed (50).setRing1MaxSpeed (150).setLevelUpCount (5)
 			.setTimeOut(1000).build ();
 
-		
 		allLevels [1] = builder.setLevel (new Level ()).setRing1 (allRings [0]).setLevelNumber (1).setRing1Direction(1.0f)
 			.setRing1AnimType(EGTween.EaseType.linear).setRing1MinSpeed (50).setRing1MaxSpeed (150).setLevelUpCount (5)
 			.setTimeOut(1000).build ();
@@ -107,12 +106,29 @@ public class LevelManager : MonoBehaviour {
 			.setRing1AnimType(EGTween.EaseType.linear).setRing1MinSpeed (50).setRing1MaxSpeed (150).setLevelUpCount (5)
 			.setTimeOut(10).build ();
 
+		//beğenmezsen uçur reyiz --mustafa
 		allLevels [10] = builder.setLevel (new Level ()).setRing1 (allRings [1]).setRing2(allRings[5]).setLevelNumber (1)
 			.setRing1Direction(1.0f).setRing2Direction(-1.0f)
 			.setRing1AnimType(EGTween.EaseType.linear).setRing2AnimType(EGTween.EaseType.linear)
 			.setRing1MinSpeed (50).setRing2MinSpeed(20)
 			.setRing1MaxSpeed (150).setLevelUpCount (5).
 			setTimeOut(40f).build ();
+
+		//beğenmezsen uçur reyiz --mustafa
+		allLevels [11] = builder.setLevel (new Level ()).setRing1 (allRings [3]).setRing2(allRings[5]).setLevelNumber (1)
+			.setRing1Direction(1.0f).setRing2Direction(-1.0f)
+			.setRing1AnimType(EGTween.EaseType.linear).setRing2AnimType(EGTween.EaseType.linear)
+			.setRing1MinSpeed (100).setRing2MinSpeed(20)
+			.setRing1MaxSpeed (150).setLevelUpCount (5).
+			setTimeOut(1000).build ();
+
+		//beğenmezsen uçur reyiz --mustafa
+		allLevels [12] = builder.setLevel (new Level ()).setRing1 (allRings [3]).setRing2(allRings[6]).setLevelNumber (1)
+			.setRing1Direction(1.0f).setRing2Direction(-1.0f)
+			.setRing1AnimType(EGTween.EaseType.linear).setRing2AnimType(EGTween.EaseType.linear)
+			.setRing1MinSpeed (100).setRing2MinSpeed(20)
+			.setRing1MaxSpeed (150).setLevelUpCount (5).
+			setTimeOut(1000).build ();
 		
 		/*
 		allLevels [9] = builder.setLevel (new Level ()).setRing1 (allRings [0]).setLevelNumber (1).setRing1Direction(1.0f)
