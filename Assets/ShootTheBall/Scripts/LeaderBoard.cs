b3ce7a88d798520ce88c770771fd2d4c9d039963
mehmet <mehmetkurhan87@gmail.com>
2016-09-03 14:58:37 +0300

    Leaderboard test

---
+++ Assets/ShootTheBall/Scripts/LeaderBoard.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
+using GooglePlayGames.BasicApi;
+
+public class LeaderBoard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder ()
+			.EnableSavedGames ().Build ();
+		PlayGamesPlatform.InitializeInstance (config);
+		PlayGamesPlatform.DebugLogEnabled = true;
+		PlayGamesPlatform.Activate ();
+		Social.localUser.Authenticate ((bool success) => {
+
+			if(success) {
+			Debug.Log("Logged In");
+			Social.ShowLeaderboardUI ();
+			}
+		});
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
